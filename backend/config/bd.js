50cb938f23bc33945ed44ac65ece57d17fe5299a
williamndongmo88 <williamndongmo88@gmail.com>
2025-10-27 04:15:23 +0100

    Préparation du projet pour la prod sur Railway

--- backend/config/bd.js
+++ backend/config/bd.js
@@ -3,11 +3,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: process.env.DATABASE_PORT || 3306,
-  user: process.env.DATABASE_USERNAME || 'root',
-  password: process.env.DATABASE_PASSWORD || 'dev-root',
-  database: process.env.DATABASE_NAME || 'dev_artisan_des_saveurs_db',
+  host: process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQLPORT || 3306,
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'dev-root',
+  database: process.env.MYSQL_DATABASE || 'dev_artisan_des_saveurs_db',
   waitForConnections: true,
   connectionLimit: 10,
 });
