55caf5203a1a7e723c0bc0d1e0c14e3821868030
williamndongmo88 <williamndongmo88@gmail.com>
2025-10-28 04:56:27 +0100

    CI: MAJ de la config du fichier ci.yml avec les variables d'environnements:11

--- backend/config/bd.js
+++ backend/config/bd.js
@@ -3,10 +3,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  port: process.env.MYSQLPORT || 3306,
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'dev-root',
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || 3306,
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'dev-root',
   database: process.env.MYSQL_DATABASE || 'dev_artisan_des_saveurs_db',
   waitForConnections: true,
   connectionLimit: 10,
--- backend/tests/jest.setup.js
+++ backend/tests/jest.setup.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config({ path: '.env.test' });
 
 const dbConfig = {
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 };
 
 const TEST_DB_NAME = process.env.MYSQL_DATABASE;
